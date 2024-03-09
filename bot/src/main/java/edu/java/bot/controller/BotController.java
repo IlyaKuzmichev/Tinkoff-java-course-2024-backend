bd1903fac90eabfb3ce0002fca62e71f8d5927a8
wilmerno <kuzmichevilyaof@gmail.com>
2024-03-09 17:25:16 +0300

    Change return values in controllers to void

--- bot/src/main/java/edu/java/bot/controller/BotController.java
+++ bot/src/main/java/edu/java/bot/controller/BotController.java
@@ -1,7 +1,6 @@
 package edu.java.bot.controller;
 
 import edu.java.bot.controller.dto.LinkUpdateRequest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BotController {
     @PostMapping("/updates")
-    public ResponseEntity<Void> processUpdate(@RequestBody LinkUpdateRequest linkUpdateRequest) {
+    public void processUpdate(@RequestBody LinkUpdateRequest linkUpdateRequest) {
         // Update processing logic
-        return ResponseEntity.ok().build();
     }
 }
--- scrapper/src/main/java/edu/java/controller/ChatController.java
+++ scrapper/src/main/java/edu/java/controller/ChatController.java
@@ -1,6 +1,5 @@
 package edu.java.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,14 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tg-bot")
 public class ChatController {
     @PostMapping("/{id}")
-    public ResponseEntity<Void> registerChat(@PathVariable Long id) {
+    public void registerChat(@PathVariable Long id) {
         // registration of new chat in repository
-        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteChat(@PathVariable Long id) {
+    public void deleteChat(@PathVariable Long id) {
         // deletion of the chat from repository
-        return ResponseEntity.ok().build();
     }
 }
